9d9138899538497fee2e2803eaed169ce54b8aa7
SwallowChen <Swallow@SwallowChendeMacBook-Pro.local>
2016-06-08 20:40:37 +0800

    Initial commit

---
+++ app/src/main/java/com/studio/swallowcharchar/happybirthday2016/MainActivity.java
@@ -0,0 +1,13 @@
+package com.studio.swallowcharchar.happybirthday2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
